f1b34f0a30c91ebc3f22e6cb7a5334e474128957
assadan <danielassa411@gmail.com>
2022-07-19 14:58:17 +0300

    Fix for the IrisComp.cpp implementation

--- IrisComp.cpp
+++ IrisComp.cpp
@@ -4,9 +4,9 @@
 
 #include "IrisComp.hpp"
 IrisComp ::IrisComp(Iris iris, double dis) : iris(iris), dis(dis){};
-bool IrisComp ::operator!=(const IrisComp &irisComp) const {return this->dis != dis;}
-bool IrisComp ::operator<=(const IrisComp &irisComp) const {return this->dis <= dis;}
-bool IrisComp ::operator>=(const IrisComp &irisComp) const {return this->dis >= dis;}
-bool IrisComp ::operator==(const IrisComp &irisComp) const {return this->dis == dis;}
-bool IrisComp ::operator<(const IrisComp &irisComp) const {return this->dis < dis;}
-bool IrisComp ::operator>(const IrisComp &irisComp) const {return this->dis > dis;}
+bool IrisComp ::operator!=(const IrisComp &irisComp) const {return this->dis != irisComp.dis;}
+bool IrisComp ::operator<=(const IrisComp &irisComp) const {return this->dis <= irisComp.dis;}
+bool IrisComp ::operator>=(const IrisComp &irisComp) const {return this->dis >= irisComp.dis;}
+bool IrisComp ::operator==(const IrisComp &irisComp) const {return this->dis == irisComp.dis;}
+bool IrisComp ::operator<(const IrisComp &irisComp) const {return this->dis < irisComp.dis;}
+bool IrisComp ::operator>(const IrisComp &irisComp) const {return this->dis > irisComp.dis;}
